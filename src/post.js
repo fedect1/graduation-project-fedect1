42cbd95fc2d8f1ce433d2a7f1c80be1757171bf5
Federico Andres Carrillo <136801767+fedect1@users.noreply.github.com>
2023-07-14 07:32:46 +0000

    Refactor of the User and Post classes

--- src/post.js
+++ src/post.js
@@ -1,18 +1,8 @@
 class Post {
-  constructor() {
-    this.posts = []
-  }
-  addPost(username, date, hour, textPost) {
-    this.posts.push({ username, date, hour, textPost })
-  }
-  getPosts() {
-    return this.posts
-      .map((post, i) => {
-        return `\nUsername: ${post.username}\nIndex: ${i + 1}\nDate: ${post.date}\nHour: ${post.hour}\nPost: ${
-          post.textPost
-        }\n\n`
-      })
-      .join('')
+  constructor(date, hour, message) {
+    this.date = date
+    this.hour = hour
+    this.message = message
   }
 }
 
--- src/user.js
+++ src/user.js
@@ -1,3 +1,5 @@
+const Post = require('./post')
+
 class User {
   constructor(name, username, email, location, age) {
     this.name = name
@@ -5,14 +7,26 @@ class User {
     this.email = email
     this.location = location
     this.age = age
+    this.collectionPost = []
   }
-  getInfo() {
-    return `${this.name} is in ${this.location}.
-    He/She/They registered with the following
-    - email: ${this.email}
-    - Date of birth: ${this.age}
-    - Username: ${this.username}
-    `
+
+  post(date, hour, message) {
+    const newPost = new Post(date, hour, message)
+    this.collectionPost.push(newPost)
+  }
+
+  get info() {
+    return `\n${this.name} is in ${this.location}.\nHe/She/They registered with the following data\n- email: ${
+      this.email
+    }\n- Date of birth: ${this.age}\n- Username: ${
+      this.username
+    }\n\nHe/She/They posted the following messages:${this.collectionPost
+      .map((post, i) => {
+        return `\n\n*****  Post number: ${i + 1}  *****\nDate: ${post.date}\nHour: ${post.hour}\nMessage: ${
+          post.message
+        }`
+      })
+      .join('')}`
   }
 }
 
