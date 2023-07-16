24eef7d1ebf0317b34b004e601ed3cc38f87f16a
Federico Andres Carrillo <136801767+fedect1@users.noreply.github.com>
2023-07-16 14:03:37 +0000

    Add method deletePost

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 //Domain: DW Messenger
 const User = require('./user')
-const Post = require('./post')
 
 //User 1
-const fede = new User('Federico', 'fedect1', 'fede@gmail.com', 'Berlin', 35)
+const fede = new User('Federico', 'fedect1', 'fede@gmail.com', 'validPASS123')
 
 fede.post('08/07/2023', '22:30', 'This is my first message')
 fede.post('09/07/2023', '03:30', 'Checking if it is working')
-//fede.deletePost('post')
+fede.deletePost(2)
 console.log(fede.info)
--- src/user.js
+++ src/user.js
@@ -1,12 +1,11 @@
 const Post = require('./post')
 
 class User {
-  constructor(name, username, email, location, age) {
+  constructor(name, username, email, password) {
     this.name = name
     this.username = username
     this.email = email
-    this.location = location
-    this.age = age
+    this.password = password
     this.collectionPost = []
   }
 
@@ -15,16 +14,18 @@ class User {
     this.collectionPost.push(newPost)
   }
 
+  deletePost(indexPost) {
+    if (indexPost >= 0 && indexPost < this.collectionPost.length) {
+      this.collectionPost.splice(indexPost, 1)
+    }
+  }
+
   get info() {
-    return `\n${this.name} is in ${this.location}.\nHe/She/They registered with the following data\n- email: ${
-      this.email
-    }\n- Date of birth: ${this.age}\n- Username: ${
-      this.username
-    }\n\nHe/She/They posted the following messages:${this.collectionPost
+    return `\n${this.name}.\nHe/She/They registered with the following data\n- email: ${this.email}\n- Password: ${
+      this.password
+    }\n- Username: ${this.username}\n\nHe/She/They posted the following messages:${this.collectionPost
       .map((post, i) => {
-        return `\n\n*****  Post number: ${i + 1}  *****\nDate: ${post.date}\nHour: ${post.hour}\nMessage: ${
-          post.message
-        }`
+        return `\n\n*****  Post number: ${i}  *****\nDate: ${post.date}\nHour: ${post.hour}\nMessage: ${post.message}`
       })
       .join('')}`
   }
