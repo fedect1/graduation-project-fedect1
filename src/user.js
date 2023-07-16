e7e7a7755654f7203ddefedf9f578e0160df4873
Federico Andres Carrillo <136801767+fedect1@users.noreply.github.com>
2023-07-16 15:16:32 +0000

    Install npm password-validator and validate the User password

--- src/user.js
+++ src/user.js
@@ -1,11 +1,30 @@
 const Post = require('./post')
-
+const passwordValidator = require('password-validator')
+let userPassword = new passwordValidator()
+userPassword
+  .is()
+  .min(8) // Minimum length 8
+  .is()
+  .max(100) // Maximum length 100
+  .has()
+  .uppercase() // Must have uppercase letters
+  .has()
+  .lowercase() // Must have lowercase letters
+  .has()
+  .digits(2) // Must have at least 2 digits
+  .has()
+  .not()
+  .spaces() // Should not have spaces
 class User {
   #password
+
   constructor(name, username, email, password) {
     this.name = name
     this.username = username
     this.email = email
+    if (!userPassword.validate(password)) {
+      throw new Error('The password does not meet the minimum requirements.')
+    }
     this.#password = password
     this.collectionPost = []
   }
@@ -22,6 +41,9 @@ class User {
   }
 
   set password(newPassword) {
+    if (!userPassword.validate(newPassword)) {
+      throw new Error('The password does not meet the minimum requirements.')
+    }
     this.#password = newPassword
   }
 
