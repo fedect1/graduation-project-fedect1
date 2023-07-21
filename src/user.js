f1de228695ea4f31549d6e15d1beb3baf9898ab0
Federico Andres Carrillo <136801767+fedect1@users.noreply.github.com>
2023-07-21 09:59:17 +0000

    Refactoring of the User class. Removal of the password property. Adding the interaction and profile property

--- src/user.js
+++ src/user.js
@@ -1,62 +1,29 @@
 const Post = require('./post')
-const passwordValidator = require('password-validator')
-let userPassword = new passwordValidator()
-userPassword
-  .is()
-  .min(8) // Minimum length 8
-  .is()
-  .max(100) // Maximum length 100
-  .has()
-  .uppercase() // Must have uppercase letters
-  .has()
-  .lowercase() // Must have lowercase letters
-  .has()
-  .digits(2) // Must have at least 2 digits
-  .has()
-  .not()
-  .spaces() // Should not have spaces
-class User {
-  #password
+const Profile = require('./profile')
 
-  constructor(name, username, email, password) {
-    this.name = name
-    this.username = username
+class User {
+  posts = []
+  interaction = { following: [], followed: [] }
+  constructor(email) {
     this.email = email
-    if (!userPassword.validate(password)) {
-      throw new Error('The password does not meet the minimum requirements.')
-    }
-    this.#password = password
-    this.collectionPost = []
+    this.profile = new Profile(email)
   }
 
-  post(date, hour, message) {
-    const newPost = new Post(date, hour, message)
-    this.collectionPost.push(newPost)
+  post(message) {
+    const newPost = new Post(message)
+    this.posts.push(newPost)
   }
 
   deletePost(indexPost) {
-    if (indexPost >= 0 && indexPost < this.collectionPost.length) {
-      this.collectionPost.splice(indexPost, 1)
+    if (indexPost >= 0 && indexPost < this.posts.length) {
+      this.posts.splice(indexPost, 1)
     } else {
       throw new Error('The index you entered does not correspond to the length of the array.')
     }
   }
 
-  set password(newPassword) {
-    if (!userPassword.validate(newPassword)) {
-      throw new Error('The password does not meet the minimum requirements.')
-    }
-    this.#password = newPassword
-  }
-
   get info() {
-    return `\n${this.name} is registered with the following data\n- email: ${this.email}\n- Password: ${
-      this.#password
-    }\n- Username: ${this.username}\n\nAnd posted the following messages:${this.collectionPost
-      .map((post, i) => {
-        return `\n\n*****  Post number: ${i}  *****\nDate: ${post.date}\nHour: ${post.hour}\nMessage: ${post.message}`
-      })
-      .join('')}`
+    return this.profile
   }
 }
 
