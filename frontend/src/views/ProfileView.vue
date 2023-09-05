b1faac6c97721b3c3770e11fe1511a9d9529a5ca
Federico Andres Carrillo <136801767+fedect1@users.noreply.github.com>
2023-09-05 08:10:51 +0000

    Style profile edit cards

--- frontend/src/views/ProfileView.vue
+++ frontend/src/views/ProfileView.vue
@@ -9,15 +9,15 @@ export default {
   name: 'ProfileView',
   data() {
     return {
-      newUsername: '',
-      newDescription: '',
-      newAvatar: '',
+      newUsername: "",
+      newDescription: "",
       posts: []
     }
   },
   async mounted() {
     this.posts = await this.fetchUserPosts(this.user)
-    console.log(this.posts)
+    this.newUsername = this.user.name
+    this.newDescription = this.user.description
   },
 
   computed: {
@@ -38,47 +38,87 @@ export default {
       this.newDescription = ''
       await this.fetchUser()
     },
-    async handleAvatarChange() {
-      const newAvatar = this.newAvatar
-      await this.updateAvatar(this.user, newAvatar)
-      this.newAvatar = ''
-      await this.fetchUser()
+
+    formatDay(dateStr) {
+      const date = new Date(dateStr)
+      if (isNaN(date.getTime())) {
+        return 'Invalid Date'
+      }
+      const dayPassed = Math.floor((Date.now() - date.getTime()) / (1000 * 60 * 60 * 24))
+      if (dayPassed === 0) {
+        const hourPassed = Math.floor((Date.now() - date.getTime()) / (1000 * 60 * 60))
+        if (hourPassed === 0) {
+          const minutePassed = Math.floor((Date.now() - date.getTime()) / (1000 * 60))
+          if (minutePassed === 0) {
+            return 'Just now'
+          } else if (minutePassed === 1) {
+            return '1 minute ago'
+          } else {
+            return `${minutePassed} minutes ago`
+          }
+        } else if (hourPassed === 1) {
+          return '1 hour ago'
+        } else {
+          return `${hourPassed} hours ago`
+        }
+      } else if (dayPassed === 1) {
+        return 'Yesterday'
+      } else if (dayPassed < 7) {
+        return `${dayPassed} days ago`
+      } else if (dayPassed < 30) {
+        return `${Math.floor(dayPassed / 7)} weeks ago`
+      } else if (dayPassed < 365) {
+        return `${Math.floor(dayPassed / 30)} months ago`
+      } else {
+        return `${Math.floor(dayPassed / 365)} years ago`
+      }
     },
   }
 }
 </script>
 
 <template>
+
   <div class="profile-edit-container">
-    <div class="username">
-      <p>Username</p>
-      <input
-        v-model="newUsername"
-        type="text"
-        name="username"
-        id="username"
-        placeholder="Username"
-      />
-      <button @click="handleNameChange">Save</button>
+    <div class="card">
+      <div class="card-content">
+        <div class="username">
+          <p>Username</p>
+          <input
+            v-model="newUsername"
+            type="text"
+            name="username"
+            id="username"
+            :placeholder="newUsername"
+          />
+          <button @click="handleNameChange">Save</button>
+        </div>
+        <div class="description">
+          <p>Description</p>
+          <input
+            v-model="newDescription"
+            type="text"
+            name="description"
+            id="description"
+            :placeholder="newDescription"
+          />
+          <button @click="handleDescriptionChange">Save</button>
+        </div>
+      </div>
     </div>
-    <div class="description">
-      <p>Description</p>
-      <input
-        v-model="newDescription"
-        type="text"
-        name="description"
-        id="description"
-        placeholder="Description"
-      />
-      <button @click="handleDescriptionChange">Save</button>
-    </div>
+  </div>
     <div class="list-post">
       <p>Posts</p>
       <div v-for="post in posts" :key="post._id">
         <p>{{ post.body }}</p>
+        <p>{{ formatDay(post.createdAt) }}</p>
+        <div v-for="comment in post.comments">
+          <p>{{ comment.author }}</p>
+          <p>{{ comment.text }}</p>
+        </div>
       </div>
     </div>
-  </div>
+
 
 </template>
 
