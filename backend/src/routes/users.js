2014548c02ef2f6afd8d182e176a4b85d50d9c0c
Federico Andres Carrillo <136801767+fedect1@users.noreply.github.com>
2023-09-07 14:20:21 +0000

    Profile tests

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -87,58 +87,58 @@ router.delete('/profile/:userId/unfollowing/:unfollowId', async function (req, r
 })
 
 /* GET followers of a user. */
-router.get('/profile/:userId/followers', async function (req, res, next) {
-  try {
-    const user = await User.findById(req.params.userId)
-    if (!user) {
-      return res.status(404).send({ message: 'User not found' })
-    }
-    const followers = await user.following
-    res.status(200).send({ followers })
-  } catch (error) {
-    res.status(404).send(error.message)
-  }
-})
+// router.get('/profile/:userId/followers', async function (req, res, next) {
+//   try {
+//     const user = await User.findById(req.params.userId)
+//     if (!user) {
+//       return res.status(404).send({ message: 'User not found' })
+//     }
+//     const followers = await user.following
+//     res.status(200).send({ followers })
+//   } catch (error) {
+//     res.status(404).send(error.message)
+//   }
+// })
 
 /* GET followings of a user. */
-router.get('/profile/:userId/followings', async function (req, res, next) {
-  try {
-    const user = await User.findById(req.params.userId)
-    if (!user) {
-      return res.status(404).send({ message: 'User not found' })
-    }
-    const followings = user.following
-    res.status(200).send({ followings })
-  } catch (error) {
-    res.status(404).send(error.message)
-  }
-})
+// router.get('/profile/:userId/followings', async function (req, res, next) {
+//   try {
+//     const user = await User.findById(req.params.userId)
+//     if (!user) {
+//       return res.status(404).send({ message: 'User not found' })
+//     }
+//     const followings = user.following
+//     res.status(200).send({ followings })
+//   } catch (error) {
+//     res.status(404).send(error.message)
+//   }
+// })
 
 //Profile Routes
 /* Get profile by id. */
-router.get('/profile/:userId', async function (req, res, next) {
-  const user = await User.findById(req.params.userId)
-  const profile = {
-    name: user.name,
-    description: user.description,
-    avatar: user.avatar,
-  }
-  res.status(200).send(profile)
-}),
-  /* Update name of a user */
-  router.put('/profile/:userId/name', async function (req, res, next) {
-    try {
-      const user = await User.findById(req.params.userId)
-      if (!user) {
-        return res.status(404).send({ message: 'User not found' })
-      }
-      user.name = req.body.name
-      await user.save()
-      res.status(204).send(user)
-    } catch (error) {
-      res.status(404).send(error.message)
+// router.get('/profile/:userId', async function (req, res, next) {
+//   const user = await User.findById(req.params.userId)
+//   const profile = {
+//     name: user.name,
+//     description: user.description,
+//     avatar: user.avatar,
+//   }
+//   res.status(200).send(profile)
+// }),
+/* Update name of a user */
+router.put('/profile/:userId/name', async function (req, res, next) {
+  try {
+    const user = await User.findById(req.params.userId)
+    if (!user) {
+      return res.status(404).send({ message: 'User not found' })
     }
-  })
+    user.name = req.body.name
+    await user.save()
+    res.status(204).send(user)
+  } catch (error) {
+    res.status(404).send(error.message)
+  }
+})
 
 /* Update description of a user */
 router.put('/profile/:userId/description', async function (req, res, next) {
--- backend/test/user.test.js
+++ backend/test/user.test.js
@@ -65,3 +65,65 @@ describe('Create user test', () => {
     expect(response.body.validation.body.message).toEqual(expectedOutput.message)
   })
 })
+
+describe('Profile routes', () => {
+  let testUser
+
+  beforeEach(async () => {
+    await User.deleteMany()
+
+    testUser = await User.create({
+      name: 'Fede',
+      email: 'fede@gmail.com',
+      password: '123456',
+    })
+
+    userToFollow = await User.create({
+      name: 'Raul',
+      email: 'raul@gmail.com',
+      password: '123456',
+    })
+  })
+  afterEach(async () => {
+    await User.deleteMany()
+  })
+
+  it('should update name of user', async () => {
+    const newName = 'Federico'
+    const expectedOutput = {
+      name: 'Federico',
+    }
+    const response = await request(app).put(`/users/profile/${testUser._id}/name`).send({ name: newName })
+    expect(response.status).toEqual(204)
+    expect(response.body).toEqual({})
+  })
+
+  it('should update description of user', async () => {
+    const newDescription = 'This is a description'
+    const expectedOutput = {
+      description: 'This is a description',
+    }
+    const response = await request(app)
+      .put(`/users/profile/${testUser._id}/description`)
+      .send({ description: newDescription })
+    expect(response.status).toEqual(204)
+    expect(response.body).toEqual({})
+  })
+
+  it('should allow the current user to follow another user', async () => {
+    const response = await request(app)
+      .put(`/users/profile/${testUser._id}/follow`)
+      .send({ userToFollow: userToFollow._id })
+    expect(response.status).toBe(201)
+    //Verify that the user is following the other user
+    const updatedTestUser = await User.findById(testUser._id)
+    expect(updatedTestUser.following.toString()).toContain(userToFollow._id.toString())
+    //Verify that the other user has a new follower
+    const updatedUserToFollow = await User.findById(userToFollow._id)
+    expect(updatedUserToFollow.followedBy.toString()).toContain(testUser._id.toString())
+  })
+  it('should allow the current user to unfollow another user', async () => {
+    const response = await request(app).delete(`/users/profile/${testUser._id}/unfollowing/${userToFollow._id}`).send()
+    expect(response.status).toBe(204)
+  })
+})
